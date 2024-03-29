f717561817cb88df4de6d71b2d8a080a7927a2ce
Diego Aldao <diegofrontdev@gmail.com>
2024-03-29 20:59:35 -0300

    añadido el section del hero y eliminadas clases innecesarias del main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import MainContent from "@/components/PageInicio/Hero/MainContent";
+
 export default function Home() {
   return (
-    <main className="w-full h-[100vh] bg-[#101010]">
-      <p className="uppercase font-bold text-main-color text-5xl">movieverse</p>
+    <main className="w-full">
+      <section className="hero h-[110vh] min-h-[830px] relative">
+        <span className="absolute top-28 md:top-32 lg:top-36 z-20 left-4 md:left-8 lg:left-10 uppercase text-xs md:text-sm bg-[#2425267e] py-2 px-4 w-fit rounded-full">
+          últimos estrenos
+        </span>
+
+        <MainContent />
+        <button className="absolute z-20 bottom-24 left-[calc(50%-50px)] lg:left-[calc(50%-65px)] flex items-center gap-1 py-2 lg:px-4 px-3">
+          <span className="capitalize ml-auto md:text-lg lg:text-xl">
+            explorar
+          </span>
+          <span className="icon-[mdi--arrow-down-thin] h-5 w-5 md:h-6 md:w-6 lg:h-7 lg:w-7"></span>
+        </button>
+      </section>
     </main>
   );
 }
