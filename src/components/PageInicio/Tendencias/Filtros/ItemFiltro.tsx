693600297315daf41405960fbd0909ac716c24b9
Diego Aldao <diegofrontdev@gmail.com>
2024-04-01 22:17:55 -0300

    añadido un componente reutilizable para renderizar cada filtro de la seccion tendencia de la pagina principal

---
+++ src/components/PageInicio/Tendencias/Filtros/ItemFiltro.tsx
@@ -0,0 +1,30 @@
+interface Props {
+  currentFiltro: string;
+  valorFiltro: string;
+  handleClick: (valorFiltro: string, nombreFiltro: string) => void;
+  nombreFiltro: string;
+  categoriaFiltro: string;
+}
+
+export default function ItemFiltro({
+  currentFiltro,
+  valorFiltro,
+  handleClick,
+  nombreFiltro,
+  categoriaFiltro,
+}: Props) {
+  return (
+    <span
+      className={`px-4 text-xs lg:text-sm py-1 capitalize  rounded-full transition-colors ${
+        currentFiltro === valorFiltro
+          ? "bg-main-color text-main-black"
+          : "bg-[#b0b3b82c]"
+      }`}
+      onClick={() => {
+        handleClick(valorFiltro, categoriaFiltro);
+      }}
+    >
+      {nombreFiltro}
+    </span>
+  );
+}
