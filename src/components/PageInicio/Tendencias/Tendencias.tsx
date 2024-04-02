69b0f109dd29ace65654062ce338bfd0bb491c55
Diego Aldao <diegofrontdev@gmail.com>
2024-04-01 22:15:34 -0300

    añadida la seccion tendencias de la pagina principal, con estados para los filtros y la url, y un useEffect para actualizar la url cuando los filtros cambian

---
+++ src/components/PageInicio/Tendencias/Tendencias.tsx
@@ -0,0 +1,33 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import Filtros from "./Filtros/Filtros";
+import ContentTendencias from "./ContentTendencias";
+import { baseUrlTendencias, lenguajeEsp } from "@/constants/constants";
+import { Filtro } from "@/types/localTypes";
+import CustomSection from "@/containers/PageInicio.tsx/CustomSection";
+
+const initialFiltros: Filtro = {
+  tiempo: "day",
+  tipo: "all",
+};
+const initialUrl: string = `${baseUrlTendencias}/all/day${lenguajeEsp}`;
+
+export default function Tendencias() {
+  const [filtros, setFiltros] = useState<Filtro>(initialFiltros);
+  const [url, setUrl] = useState<string>(initialUrl);
+
+  useEffect(() => {
+    setUrl(
+      `${baseUrlTendencias}/${filtros.tipo}/${filtros.tiempo}${lenguajeEsp}`
+    );
+  }, [filtros]);
+
+  return (
+    <CustomSection
+      titulo="tendencias"
+      headerChildren={<Filtros filtros={filtros} setFiltros={setFiltros} />}
+    >
+      <ContentTendencias url={url} />
+    </CustomSection>
+  );
+}
