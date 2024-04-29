40936f966b86c0e0ccb21d49eff3c0d7eceba0ef
Diego Aldao <diegofrontdev@gmail.com>
2024-04-29 19:01:24 -0300

    corregido el z-index de la card principal

--- src/components/cards/MainCard.tsx
+++ src/components/cards/MainCard.tsx
@@ -115,7 +115,7 @@ export default function MainCard({
           className="w-full h-full object-cover"
         />
       </div>
-      <div className="contenido rounded-md w-full h-full absolute top-0 left-0 z-20 after:rounded-md after:inset-[3px] md:after:inset-[4px] after:absolute after:bg-gradient-to-t after:from-[#101010] after:via-transparent after:to-transparent flex flex-col justify-between p-2 md:p-3 xl:p-4">
+      <div className="contenido rounded-md w-full h-full absolute top-0 left-0 z-[2] after:rounded-md after:inset-[3px] md:after:inset-[4px] after:absolute after:bg-gradient-to-t after:from-[#101010] after:via-transparent after:to-transparent flex flex-col justify-between p-2 md:p-3 xl:p-4">
         <header className="flex items-center justify-end ">
           <div className="menu bg-[#10101017] rounded-full h-7 w-7 xl:h-8 xl:w-8 flex items-center justify-center backdrop-blur-xl">
             <span className="icon-[mdi--dots-vertical]"></span>
