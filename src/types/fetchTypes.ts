9624c8d026922d84dd2e75c46d7128cf0375bc05
Diego Aldao <diegofrontdev@gmail.com>
2024-04-30 19:09:08 -0300

    añadidos tipos para los fetchs de series, series y peliculas, y celebridades

--- src/types/fetchTypes.ts
+++ src/types/fetchTypes.ts
@@ -104,3 +104,68 @@ export enum KnownForDepartment {
 export enum ResultMediaType {
   Person = "person",
 }
+
+export interface Series {
+  page: number;
+  results: Serie[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface Serie {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview?: string;
+  popularity?: number;
+  poster_path?: string;
+  first_air_date?: Date | string;
+  name?: string;
+  vote_average?: number;
+  vote_count?: number;
+}
+
+export interface Audiovisual {
+  adult: boolean;
+  backdrop_path: string;
+  id: number;
+  title?: string;
+  original_language: string;
+  original_title?: string;
+  overview: string;
+  poster_path: string;
+  media_type?: string;
+  genre_ids: number[];
+  popularity: number;
+  release_date?: Date;
+  video?: boolean;
+  vote_average: number;
+  vote_count: number;
+  name?: string;
+  original_name?: string;
+  first_air_date?: Date;
+  origin_country?: string[];
+}
+
+export interface Celebridades {
+  page: number;
+  results: Celebridad[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface Celebridad {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: null | string;
+  known_for: KnownFor[];
+}
