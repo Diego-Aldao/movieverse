e21d9114721898bf4f21c5667c945a83f80b6059
Diego Aldao <diegofrontdev@gmail.com>
2024-05-01 18:59:27 -0300

    quitada una clase tailwind innecesaria

--- src/components/PagesPeliculasSeries/Filtros/FilterGroup.tsx
+++ src/components/PagesPeliculasSeries/Filtros/FilterGroup.tsx
@@ -13,7 +13,7 @@ export default function FilterGroup({
 }: Props) {
   return (
     <div
-      className={`bg-secondary-black z-[3] w-full sm:absolute top-12 left-0 transition-all rounded-sm  ${
+      className={`bg-secondary-black z-[3] w-full sm:absolute left-0 transition-all rounded-sm  ${
         filtroVisible
           ? "visible opacity-100 h-fit p-2 py-4 sm:p-4 sm:py-6"
           : "invisible opacity-0 h-0 p-0"
