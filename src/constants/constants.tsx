bd56be79bb57469c7ed395e3c258a729687c9a1b
Diego Aldao <diegofrontdev@gmail.com>
2024-05-02 19:02:22 -0300

    añadida una costante para la url base de la api de detalle de peliculas

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -1,14 +1,21 @@
 export const BASE_URL_DISCOVER_MOVIE =
   "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=es-MX&";
+
 export const BASE_URL_DISCOVER_SERIES =
   "https://api.themoviedb.org/3/discover/tv?include_adult=false&include_video=false&language=es-MX&";
+
 export const BASE_URL_POPULAR_PEOPLE =
   "https://api.themoviedb.org/3/person/popular?language=es-MX&";
+
 export const BASE_URL_TRENDING = "https://api.themoviedb.org/3/trending";
+
 export const BASE_URL_ESTRENOS =
   "https://api.themoviedb.org/3/movie/now_playing?language=es-MX&page=1";
+
 export const BASE_URL_TOP_SERIES =
   "https://api.themoviedb.org/3/discover/tv?include_adult=true&language=es-MX&page=1&sort_by=vote_average.desc&vote_count.gte=2000";
+
+export const BASE_URL_MOVIE_DETAIL = "https://api.themoviedb.org/3/movie/";
 
 export const LENGUAJE_ESP = "?language=es-MX";
 
