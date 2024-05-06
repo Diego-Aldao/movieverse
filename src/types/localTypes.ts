437918396324dd6b5c48b834a6339ff33477a2e2
Diego Aldao <diegofrontdev@gmail.com>
2024-05-06 20:42:18 -0300

    añadidos types para redes sociales filtradas y para miniaturas de la seccion media de la pagina de detalle multimedia

--- src/types/localTypes.ts
+++ src/types/localTypes.ts
@@ -2,3 +2,14 @@ export interface Filtro {
   tiempo: string;
   tipo: string;
 }
+export interface MiniaturaMedia {
+  id: string;
+  key: string;
+  imagenPequeña: string;
+  imagenGrande: string;
+  nombre: string;
+}
+
+export interface RedesSociales {
+  [key: string]: string | null | number;
+}
