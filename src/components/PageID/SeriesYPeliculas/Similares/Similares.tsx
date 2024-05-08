7beb8308c0574a46e53b9806a6791264604220c1
Diego Aldao <diegofrontdev@gmail.com>
2024-05-08 19:10:49 -0300

    ahora el componente recibe directamente la data por props

--- src/components/PageID/SeriesYPeliculas/Similares/Similares.tsx
+++ src/components/PageID/SeriesYPeliculas/Similares/Similares.tsx
@@ -1,52 +1,44 @@
 import React from "react";
 import Image from "next/image";
-import {
-  BASE_URL_IMAGES,
-  BASE_URL_MOVIE_DETAIL,
-  TAMAÑOS_IMAGENES,
-} from "@/constants/constants";
+import { BASE_URL_IMAGES, TAMAÑOS_IMAGENES } from "@/constants/constants";
 import CustomSection from "@/components/containers/PageDetalleMultimedia/CustomSection";
-import FetchDataClient from "@/data/fetchDataClient";
-import type { Similares } from "@/types/fetchTypes";
-import SkeletonSimilares from "@/components/skeletons/PagePeliculasSeries/SkeletonSimilares";
+
+import type { Reviews, Similares } from "@/types/fetchTypes";
 
 interface Props {
-  id: string | string[];
+  similares: Reviews;
 }
 
-export default function Similares({ id }: Props) {
-  const { data: similares, loading } = FetchDataClient<Similares>(
-    `${BASE_URL_MOVIE_DETAIL}${id}/similar`
-  );
+export default function Similares({ similares }: Props) {
+  const similaresFiltrados = similares.results
+    .filter((similar) => similar.backdrop_path)
+    .slice(0, 8);
   return (
     <CustomSection
       titulo="similares"
       customStyles="lg:col-start-2"
       asideSection={true}
     >
-      {loading && <SkeletonSimilares />}
-      {similares && (
-        <div className="main-content grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-1">
-          {similares.results.slice(0, 8).map((similar) => (
-            <div
-              key={similar.id}
-              className="relative flex items-center justify-center after:absolute after:inset-0 after:bg-[#101010c7] rounded overflow-hidden max-h-[100px] lg:max-h-[150px]"
-            >
-              <span className="absolute font-semibold z-[2] text-xs md:text-sm w-full text-center line-clamp-1 px-2 lg:text-base xl:text-lg">
-                {similar.title}
-              </span>
-              <Image
-                src={`${BASE_URL_IMAGES}${TAMAÑOS_IMAGENES.pequeño}${similar.backdrop_path}`}
-                alt=""
-                width={0}
-                height={0}
-                sizes="100vw"
-                className="w-full h-full object-cover"
-              />
-            </div>
-          ))}
-        </div>
-      )}
+      <div className="main-content grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-1">
+        {similaresFiltrados.map((similar) => (
+          <div
+            key={similar.id}
+            className="relative flex items-center justify-center after:absolute after:inset-0 after:bg-[#101010c7] rounded overflow-hidden max-h-[100px] lg:max-h-[150px]"
+          >
+            <span className="absolute font-semibold z-[2] text-xs md:text-sm w-full text-center line-clamp-1 px-2 lg:text-base xl:text-lg">
+              {similar.name}
+            </span>
+            <Image
+              src={`${BASE_URL_IMAGES}${TAMAÑOS_IMAGENES.pequeño}${similar.backdrop_path}`}
+              alt=""
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="w-full h-full object-cover"
+            />
+          </div>
+        ))}
+      </div>
     </CustomSection>
   );
 }
