67a2e1d0e91d8c1f5673c70d4b0dffe55793a8fd
Diego Aldao <diegofrontdev@gmail.com>
2024-05-12 21:56:21 -0300

    ahora es el propio componente el que hace el fetch y renderiza la data. Añadido un skeleton para mostrar mientras se completa el fetch

--- src/components/PageInicio/Hero/Slider.tsx
+++ src/components/PageInicio/Hero/Slider.tsx
@@ -1,9 +1,13 @@
 "use client";
 import React from "react";
 import ImagenesSlide from "./ImagenesSlide";
-import { Estreno } from "@/types/fetchTypes";
+import { Estrenos } from "@/types/fetchTypes";
 import Generos from "./Generos";
 import Slide from "./Slide";
+import SkeletonHero from "@/components/skeletons/PageInicio/SkeletonHero";
+import FetchDataClient from "@/data/fetchDataClient";
+import { BASE_URL_ESTRENOS } from "@/constants/constants";
+
 //Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Parallax } from "swiper/modules";
@@ -13,11 +17,12 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-interface Props {
-  estrenos: Estreno[];
-}
-
-export default function Slider({ estrenos }: Props) {
+export default function Slider() {
+  const { data: estrenos, loading } =
+    FetchDataClient<Estrenos>(BASE_URL_ESTRENOS);
+  const estrenosFiltrados = estrenos?.results
+    .filter((estreno) => estreno.overview)
+    .slice(0, 8);
   return (
     <Swiper
       className="w-full h-full"
@@ -28,22 +33,40 @@ export default function Slider({ estrenos }: Props) {
       parallax={true}
       speed={500}
     >
-      {estrenos.slice(0, 7).map((estreno) => (
-        <SwiperSlide className="w-full h-full overflow-hidden" key={estreno.id}>
-          <Slide
-            titulo={estreno.title}
-            votoPromedio={estreno.vote_average}
-            descripcion={estreno.overview}
-            id={estreno.id}
-            generos={<Generos generos={estreno.genre_ids} />}
-          >
-            <ImagenesSlide
-              imagenPoster={estreno.poster_path}
-              imagenBackdrop={estreno.backdrop_path}
-            />
-          </Slide>
-        </SwiperSlide>
-      ))}
+      {loading && (
+        <>
+          {Array(8)
+            .fill(0)
+            .map((_, i) => (
+              <SwiperSlide key={i}>
+                <SkeletonHero />
+              </SwiperSlide>
+            ))}
+        </>
+      )}
+      {estrenosFiltrados && (
+        <>
+          {estrenosFiltrados.map((estreno) => (
+            <SwiperSlide
+              className="w-full h-full overflow-hidden"
+              key={estreno.id}
+            >
+              <Slide
+                titulo={estreno.title}
+                votoPromedio={estreno.vote_average}
+                descripcion={estreno.overview}
+                id={estreno.id}
+                generos={<Generos generos={estreno.genre_ids} />}
+              >
+                <ImagenesSlide
+                  imagenPoster={estreno.poster_path}
+                  imagenBackdrop={estreno.backdrop_path}
+                />
+              </Slide>
+            </SwiperSlide>
+          ))}
+        </>
+      )}
     </Swiper>
   );
 }
