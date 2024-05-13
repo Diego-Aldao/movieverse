15c85a7e499a1d895699b2a44a0114e09a632f6f
Diego Aldao <diegofrontdev@gmail.com>
2024-05-12 22:00:33 -0300

    añadida la url para el fetch de los estrenos, agregadas las url base de detalle persona y serie, y quitado el "?" de la url de lenguaje_esp para que se pueda usar en todos los casos ( "?" e "&")

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -1,3 +1,6 @@
+export const BASE_URL_ESTRENOS =
+  "https://api.themoviedb.org/3/movie/now_playing?language=es-MX";
+
 export const BASE_URL_DISCOVER_MOVIE =
   "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=es-MX&";
 
@@ -9,15 +12,14 @@ export const BASE_URL_POPULAR_PEOPLE =
 
 export const BASE_URL_TRENDING = "https://api.themoviedb.org/3/trending";
 
-export const BASE_URL_ESTRENOS =
-  "https://api.themoviedb.org/3/movie/now_playing?language=es-MX&page=1";
-
 export const BASE_URL_TOP_SERIES =
   "https://api.themoviedb.org/3/discover/tv?include_adult=true&language=es-MX&page=1&sort_by=vote_average.desc&vote_count.gte=2000";
 
 export const BASE_URL_MOVIE_DETAIL = "https://api.themoviedb.org/3/movie/";
+export const BASE_URL_PERSON_DETAIL = "https://api.themoviedb.org/3/person/";
+export const BASE_URL_SERIE_DETAIL = "https://api.themoviedb.org/3/tv/";
 
-export const LENGUAJE_ESP = "?language=es-MX";
+export const LENGUAJE_ESP = "language=es-MX";
 
 export const BASE_URL_IMAGES = "https://image.tmdb.org/t/p/";
 
