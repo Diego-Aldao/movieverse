68c42b82223e6b2867328d779f63a19c1c1898dc
Diego Aldao <diegofrontdev@gmail.com>
2024-05-15 15:51:34 -0300

    añadido un skeleton para mostrar mientras carga la pagina Celebridades ID

---
+++ src/app/celebridades/[id]/loading.tsx
@@ -0,0 +1,6 @@
+import SkeletonCelebridad from "@/components/skeletons/PageID/SkeletonCelebridad";
+import React from "react";
+
+export default function Loading() {
+  return <SkeletonCelebridad />;
+}
