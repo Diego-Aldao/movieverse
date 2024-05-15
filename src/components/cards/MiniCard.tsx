965518db7b18b23a3d5d1adc41619c4c4ac8e451
Diego Aldao <diegofrontdev@gmail.com>
2024-05-14 22:23:44 -0300

    corregidas las clases para que el componente sea reutilizable, ya que ahora recibe el destino por props

--- src/components/cards/MiniCard.tsx
+++ src/components/cards/MiniCard.tsx
@@ -7,40 +7,32 @@ interface Props {
   pathImagen: string;
   nombre: string;
   hasButton: boolean;
-  handleClick?: (nombre: string) => void;
+  destino?: string;
 }
 
 export default function MiniCard({
   pathImagen,
   nombre,
   hasButton,
-  handleClick,
+  destino,
 }: Props) {
   return (
-    <div
-      className="w-full flex h-[120px] sm:h-full after:bg-[#242526b7] after:inset-0 after:absolute relative rounded-md overflow-hidden max-w-[370px] mx-auto sm:max-w-full md:h-full"
-      onClick={() => {
-        handleClick && handleClick(nombre);
-      }}
-    >
+    <div className="w-full flex h-[120px] sm:h-full after:bg-[#242526b7] after:inset-0 after:absolute relative rounded-md overflow-hidden max-w-[370px] mx-auto sm:max-w-full md:h-full">
       <Image
         src={`${BASE_URL_IMAGES}${TAMAÑOS_IMAGENES.pequeño}${pathImagen}`}
         alt="bg-imagen"
         width={0}
         height={0}
         sizes="100vw"
-        className="w-full h-full object-cover"
       />
-      <div className="main-content-mini w-full h-full absolute z-[2] flex flex-col p-2 sm:items-center sm:justify-center">
-        <h2 className="text-xl line-clamp-2 sm:text-lg sm:text-center">
-          {nombre}
-        </h2>
-        {hasButton && (
+      <div className="main-content-mini w-full h-full absolute z-[2] flex flex-col  p-2">
+        <h2 className="text-xl line-clamp-1 sm:text-lg md:text-xl">{nombre}</h2>
+        {hasButton && destino && (
           <MainButton
             icon="icon-[mdi--arrow-right-thin]"
             nombre="ver detalle"
-            destino="/series"
-            customStyles="mt-auto text-xs sm:hidden"
+            destino={destino}
+            customStyles="mt-auto text-xs"
           />
         )}
       </div>
