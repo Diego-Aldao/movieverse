89c5b97cc0cb37b38b334e0257224498db370c21
Diego Aldao <diegofrontdev@gmail.com>
2024-05-15 15:50:51 -0300

    agregado el children que ahora necesita el componente MainCard y cambiado el endpoint del fetch

--- src/components/PageInicio/Celebridades/Celebridades.tsx
+++ src/components/PageInicio/Celebridades/Celebridades.tsx
@@ -1,24 +1,28 @@
-import MainCard from "@/components/cards/MainCard";
+import MainCard from "@/components/cards/MainCard/MainCard";
 import fetchData from "@/data/fetchData";
 import type { Celebridad, Celebridades } from "@/types/fetchTypes";
 import React from "react";
-import { baseUrlTendencias } from "@/constants/constants";
+import { BASE_URL_POPULAR_PEOPLE } from "@/constants/constants";
+import ContentCelebridad from "@/components/cards/MainCard/ContentCelebridad";
 
 export default async function Celebridades() {
-  const url = `${baseUrlTendencias}/person/week`;
-  const data = await fetchData<Celebridades>(url);
+  const urlFetch = `${BASE_URL_POPULAR_PEOPLE}page=1&include_adult=false`;
+  const { results: celebridades } = await fetchData<Celebridades>(urlFetch);
 
   return (
-    <div className="main-content grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6  gap-2 sm:gap-5 sm:gap-y-10 md:gap-y-12 gap-y-7 xl:gap-y-16">
-      {data.results.map((tendencia: Celebridad) => (
+    <div className="main-content grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5  gap-2 sm:gap-5 sm:gap-y-10 md:gap-y-12 gap-y-7 xl:gap-y-16">
+      {celebridades.map((tendencia: Celebridad) => (
         <MainCard
           key={tendencia.id}
           imagen={tendencia.profile_path || ""}
-          nombre={tendencia.name}
-          apariciones={tendencia.known_for}
-          mediaType={"person"}
           id={tendencia.id}
-        />
+          mediaType={"person"}
+        >
+          <ContentCelebridad
+            nombre={tendencia.name}
+            apariciones={tendencia.known_for}
+          />
+        </MainCard>
       ))}
     </div>
   );
