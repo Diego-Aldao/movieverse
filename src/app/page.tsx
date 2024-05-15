79f120b13e1bc5089a409e19f853b40c2ef5b5ae
Diego Aldao <diegofrontdev@gmail.com>
2024-05-14 22:00:56 -0300

    reemplazada la seccion hero por un componente, agregados componentes suspense con un skeleton de fallback para los componentes que hacen el fetch asincrono

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,23 @@
 import Celebridades from "@/components/PageInicio/Celebridades/Celebridades";
-import MainContent from "@/components/PageInicio/Hero/ContentHero";
+import Hero from "@/components/PageInicio/Hero/Hero";
 import MejoresSeries from "@/components/PageInicio/MejoresSeries/MejoresSeries";
 import Tendencias from "@/components/PageInicio/Tendencias/Tendencias";
 import MainButton from "@/components/buttons/MainButton";
 import CustomSection from "@/components/containers/CustomSection";
+import SkeletonCelebridades from "@/components/skeletons/PageInicio/SkeletonCelebridades";
+import SkeletonMejoresSeries from "@/components/skeletons/PageInicio/SkeletonMejoresSeries";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="w-full">
-      <section className="hero h-[110vh] min-h-[830px] relative">
-        <span className="absolute top-28 md:top-32 lg:top-36 z-20 left-4 md:left-8 lg:left-10 uppercase text-xs md:text-sm bg-[#2425267e] py-2 px-4 w-fit rounded-full">
-          últimos estrenos
-        </span>
-        <MainContent />
-        <button className="absolute z-20 bottom-24 left-[calc(50%-50px)] lg:left-[calc(50%-65px)] flex items-center gap-1 py-2 lg:px-4 px-3">
-          <span className="capitalize ml-auto md:text-lg lg:text-xl">
-            explorar
-          </span>
-          <span className="icon-[mdi--arrow-down-thin] h-5 w-5 md:h-6 md:w-6 lg:h-7 lg:w-7"></span>
-        </button>
-      </section>
+      <Hero />
       <Tendencias />
       <CustomSection titulo="las mejores series">
         <>
-          <MejoresSeries />
+          <Suspense fallback={<SkeletonMejoresSeries cantidad={20} />}>
+            <MejoresSeries />
+          </Suspense>
           <MainButton
             icon="icon-[mdi--arrow-right-thin]"
             nombre="ver más series"
@@ -37,7 +31,9 @@ export default function Home() {
         titleCustomStyles="text-base sm:text-2xl"
       >
         <>
-          <Celebridades />
+          <Suspense fallback={<SkeletonCelebridades cantidad={20} />}>
+            <Celebridades />
+          </Suspense>
           <MainButton
             icon="icon-[mdi--arrow-right-thin]"
             nombre="ver más celebridades"
