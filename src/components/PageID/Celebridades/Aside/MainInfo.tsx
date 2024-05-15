a8cf12fa54280711066520e9c1dddda3a188e90c
Diego Aldao <diegofrontdev@gmail.com>
2024-05-14 22:29:56 -0300

    reubicado en una carpeta mas descriptiva. Añadidos heights minimos a la imagen, para que la ui no haga un salto cuando carga

--- src/components/PageID/Celebridades/Aside/MainInfo.tsx
+++ src/components/PageID/Celebridades/Aside/MainInfo.tsx
@@ -17,14 +17,13 @@ export default function MainInfo({
 }: Props) {
   return (
     <div className="main-info relative sm:flex sm:flex-col sm:gap-4">
-      <div className="imagen max-w-[500px] mx-auto sm:max-w-[250px] md:max-w-full w-full sm:rounded-md sm:overflow-hidden sm:after:hidden relative after:absolute after:inset-0 after:from-[#101010] after:bg-gradient-to-t after:from-10% after:to-transparent after:via-transparent">
+      <div className="imagen max-w-[500px] min-h-[480px] lg:min-h-[450px] xl:min-h-[525px] sm:min-h-[370px] mx-auto sm:max-w-[250px] md:max-w-full w-full sm:rounded-md sm:overflow-hidden sm:after:hidden relative after:absolute after:inset-0 after:from-[#101010] after:bg-gradient-to-t after:from-10% after:to-transparent after:via-transparent">
         <Image
           src={`${BASE_URL_IMAGES}${TAMAÑOS_IMAGENES.pequeño}${imageSrc}`}
           alt=""
           width={0}
           height={0}
           sizes="100vw"
-          className="w-full h-full object-cover"
         />
       </div>
       <header className="flex items-center flex-col absolute bottom-0 left-0 w-full sm:relative sm:items-start">
