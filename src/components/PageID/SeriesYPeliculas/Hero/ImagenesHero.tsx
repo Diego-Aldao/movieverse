60b694e907e1e962622c28329c09242c4cf7f6a4
Diego Aldao <diegofrontdev@gmail.com>
2024-05-16 20:32:52 -0300

    agregada una imagen mas para el tamaño sm, y corregidos los tamaños de las imagenes poster

--- src/components/PageID/SeriesYPeliculas/Hero/ImagenesHero.tsx
+++ src/components/PageID/SeriesYPeliculas/Hero/ImagenesHero.tsx
@@ -9,16 +9,26 @@ interface Props {
 
 export default function ImagenesHero({ urlPoster, urlBackdrop }: Props) {
   return (
-    <div className="absolute top-0 left-0 w-full h-full min-h-[700px] md:min-h-[600px] lg:min-h-[700px]">
+    <div className="absolute top-0 left-0 w-full h-full min-h-[700px] md:min-h-[600px] lg:min-h-[700px] overflow-hidden">
       {urlPoster ? (
-        <Image
-          src={`${BASE_URL_IMAGES}${TAMAÑOS_IMAGENES.pequeño}${urlPoster}`}
-          alt={"pelicula"}
-          width={0}
-          height={0}
-          sizes="100vw"
-          className="object-[50%,0px] md:hidden"
-        />
+        <>
+          <Image
+            src={`${BASE_URL_IMAGES}${TAMAÑOS_IMAGENES.mediano}${urlPoster}`}
+            alt={"pelicula"}
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="object-[50%,0px] sm:hidden"
+          />
+          <Image
+            src={`${BASE_URL_IMAGES}${TAMAÑOS_IMAGENES.mediano}${urlPoster}`}
+            alt={"pelicula"}
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="object-[50%,0px] hidden sm:inline-block md:hidden"
+          />
+        </>
       ) : (
         <Image
           src={`${BASE_URL_IMAGES}${TAMAÑOS_IMAGENES.pequeño}${urlBackdrop}`}
@@ -26,26 +36,18 @@ export default function ImagenesHero({ urlPoster, urlBackdrop }: Props) {
           width={0}
           height={0}
           sizes="100vw"
-          className=" object-[50%,0px] md:hidden"
+          className="object-[50%,0px] md:hidden"
         />
       )}
       {urlBackdrop ? (
         <>
-          <Image
-            src={`${BASE_URL_IMAGES}${TAMAÑOS_IMAGENES.mediano}${urlBackdrop}`}
-            alt={"pelicula"}
-            width={0}
-            height={0}
-            sizes="100vw"
-            className=" hidden md:inline-block lg:hidden 2xl:hidden object-[50%,0px]"
-          />
           <Image
             src={`${BASE_URL_IMAGES}${TAMAÑOS_IMAGENES.grande}${urlBackdrop}`}
             alt={"pelicula"}
             width={0}
             height={0}
             sizes="100vw"
-            className=" hidden lg:inline-block 2xl:hidden object-[50%,0px]"
+            className="hidden md:inline-block 2xl:hidden object-[50%,0px]"
           />
           <Image
             src={`${BASE_URL_IMAGES}${TAMAÑOS_IMAGENES.original}${urlBackdrop}`}
@@ -53,7 +55,7 @@ export default function ImagenesHero({ urlPoster, urlBackdrop }: Props) {
             width={0}
             height={0}
             sizes="100vw"
-            className=" hidden 2xl:inline-block object-[50%,0px]"
+            className="hidden 2xl:inline-block object-[50%,0px]"
           />
         </>
       ) : (
