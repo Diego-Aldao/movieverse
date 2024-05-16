62e8c54646b19959012ded3e3a51f09ecc21f64d
Diego Aldao <diegofrontdev@gmail.com>
2024-05-16 20:27:04 -0300

    añadido un skeleton para el loading de la pagina series ID

---
+++ src/app/series/[id]/loading.tsx
@@ -0,0 +1,6 @@
+import SkeletonMultimedia from "@/components/skeletons/PageID/SkeletonMultimedia";
+import React from "react";
+
+export default function Loading() {
+  return <SkeletonMultimedia />;
+}
