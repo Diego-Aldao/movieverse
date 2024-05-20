7a039d5c4422c23818bea32031a668fd48de83cd
Diego Aldao <diegofrontdev@gmail.com>
2024-05-20 19:14:59 -0300

    ahora es un server side component, agregado un min height al main y agregada la prop mediaType que ahora necesita el componente similares

--- src/app/series/[id]/page.tsx
+++ src/app/series/[id]/page.tsx
@@ -1,8 +1,5 @@
-"use client";
 import { BASE_URL_SERIE_DETAIL } from "@/constants/constants";
-import FetchDataClient from "@/data/fetchDataClient";
 import { DetalleSeries } from "@/types/fetchTypes";
-import { useParams } from "next/navigation";
 import React from "react";
 import Hero from "@/components/PageID/SeriesYPeliculas/Hero/Hero";
 import Reparto from "@/components/PageID/SeriesYPeliculas/Reparto/Reparto";
@@ -13,64 +10,58 @@ import Similares from "@/components/PageID/SeriesYPeliculas/Similares/Similares"
 import ImagenesHero from "@/components/PageID/SeriesYPeliculas/Hero/ImagenesHero";
 import HeaderHero from "@/components/PageID/SeriesYPeliculas/Hero/HeaderHero";
 import MainInfoHero from "@/components/PageID/SeriesYPeliculas/Hero/MainInfoHero";
-import SkeletonSeriesID from "@/components/skeletons/PageID/SkeletonSeriesID";
+import fetchData from "@/data/fetchData";
 
-export default function SeriesID() {
-  const { id } = useParams();
-
-  const { data: serie, loading } = FetchDataClient<DetalleSeries>(
+export default async function SeriesID({ params }: { params: { id: string } }) {
+  const { id } = params;
+  const serie = await fetchData<DetalleSeries>(
     `${BASE_URL_SERIE_DETAIL}${id}?append_to_response=aggregate_credits%2Ccontent_ratings%2Cexternal_ids%2Csimilar&language=es-MX`
   );
   return (
-    <main>
-      {loading && <SkeletonSeriesID />}
-      {serie && (
+    <main className="min-h-screen">
+      <Hero
+        descripcion={serie.overview}
+        imagenesHero={
+          <ImagenesHero
+            urlPoster={serie.poster_path}
+            urlBackdrop={serie.backdrop_path}
+          />
+        }
+      >
         <>
-          <Hero
-            descripcion={serie.overview}
-            imagenesHero={
-              <ImagenesHero
-                urlPoster={serie.poster_path}
-                urlBackdrop={serie.backdrop_path}
-              />
-            }
-          >
-            <>
-              <HeaderHero
-                titulo={serie.name}
-                tagline={serie.tagline}
-                votoPromedio={serie.vote_average}
-                cantidadDeVotos={serie.vote_count}
-              />
-              <MainInfoHero
-                generos={serie.genres}
-                estreno={serie.first_air_date}
-                duracion={serie.episode_run_time[0]}
-                edadMinima={serie.content_ratings.results[0]}
-              />
-            </>
-          </Hero>
-          <section className="main-content px-4 md:px-8 lg:px-10 max-w-7xl mx-auto 2xl:px-0 pb-20 grid w-full gap-12 lg:grid-cols-[2.5fr,1fr] lg:gap-x-4 xl:gap-x-8">
-            <Reparto cast={serie.aggregate_credits.cast} />
-            <Media
-              id={id}
-              idiomaOriginal={serie.original_language}
-              mediaType="serie"
-            />
-            <Similares similares={serie.similar} />
-            <aside className="lg:row-start-1 lg:col-start-2 flex flex-col gap-8">
-              <Redes id={id} />
-              <SubInfo
-                tituloOriginal={serie.original_name}
-                estado={serie.status}
-                idiomaOriginal={serie.original_language}
-                tipoDeSerie={serie.type}
-                esSerie={true}
-              />
-            </aside>
-          </section>
+          <HeaderHero
+            titulo={serie.name}
+            tagline={serie.tagline}
+            votoPromedio={serie.vote_average}
+            cantidadDeVotos={serie.vote_count}
+          />
+          <MainInfoHero
+            generos={serie.genres}
+            estreno={serie.first_air_date}
+            duracion={serie.episode_run_time[0]}
+            edadMinima={serie.content_ratings.results[0]}
+          />
         </>
-      )}
+      </Hero>
+      <section className="main-content px-4 md:px-8 lg:px-10 max-w-7xl mx-auto 2xl:px-0 pb-20 grid w-full gap-12 lg:grid-cols-[2.5fr,1fr] lg:gap-x-4 xl:gap-x-8">
+        <Reparto cast={serie.aggregate_credits.cast} />
+        <Media
+          id={id}
+          idiomaOriginal={serie.original_language}
+          mediaType="serie"
+        />
+        <Similares similares={serie.similar} mediaType={"tv"} />
+        <aside className="lg:row-start-1 lg:col-start-2 flex flex-col gap-8">
+          <Redes id={id} />
+          <SubInfo
+            tituloOriginal={serie.original_name}
+            estado={serie.status}
+            idiomaOriginal={serie.original_language}
+            tipoDeSerie={serie.type}
+            esSerie={true}
+          />
+        </aside>
+      </section>
     </main>
   );
 }
