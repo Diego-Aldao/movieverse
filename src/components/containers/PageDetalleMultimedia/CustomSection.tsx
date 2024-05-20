fe7efbde100c084658eaf78bb650fd7cfa46724a
Diego Aldao <diegofrontdev@gmail.com>
2024-05-20 19:18:35 -0300

    añadido un conditional rendering para cuando la seccion sea utilizada como aside

--- src/components/containers/PageDetalleMultimedia/CustomSection.tsx
+++ src/components/containers/PageDetalleMultimedia/CustomSection.tsx
@@ -5,6 +5,7 @@ interface Props {
   children: ReactNode;
   customStyles?: string;
   headerChildren?: ReactNode;
+  asideSection?: boolean;
 }
 
 export default function CustomSection({
@@ -12,11 +13,18 @@ export default function CustomSection({
   children,
   customStyles,
   headerChildren,
+  asideSection,
 }: Props) {
   return (
     <div className={`flex flex-col gap-8 min-w-full w-full ${customStyles}`}>
       <header className="flex flex-col gap-2">
-        <h2 className="text-xl uppercase md:text-2xl lg:text-3xl font-semibold">
+        <h2
+          className={`uppercase font-semibold ${
+            asideSection
+              ? "text-lg lg:text-xl"
+              : "text-xl md:text-2xl lg:text-3xl"
+          }`}
+        >
           {titulo}
         </h2>
         {headerChildren}
