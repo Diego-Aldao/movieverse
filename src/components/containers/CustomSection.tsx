d57a3ff615c1e3d93dfb85d77529eb2275c74388
Diego Aldao <diegofrontdev@gmail.com>
2024-05-21 19:29:57 -0300

    ahora el componente puede recibir por props si necesita paddings o no. Modificada la prop titleCustomStyles, ahora se llama titleCustomSizes y solo recibe tamaños para el h2

--- src/components/containers/CustomSection.tsx
+++ src/components/containers/CustomSection.tsx
@@ -4,29 +4,37 @@ interface Props {
   titulo: string;
   children: React.ReactNode;
   headerChildren?: React.ReactNode;
-  titleCustomStyles?: string;
+  titleCustomSizes?: string;
+  noInlinePadding?: boolean;
 }
 
 export default function CustomSection({
   titulo,
   children,
   headerChildren,
-  titleCustomStyles,
+  titleCustomSizes,
+  noInlinePadding,
 }: Props) {
   return (
-    <section className="px-4 md:px-8 lg:px-10 py-12 2xl:px-0">
-      <div className="series-content max-w-7xl mx-auto flex flex-col gap-12">
-        <header className="flex flex-col gap-4 md:gap-8">
-          <h2
-            className={`uppercase font-medium text-2xl lg:text-5xl md:text-4xl flex items-center justify-between gap-12 ${titleCustomStyles}`}
-          >
-            {titulo}
-            <span className="bg-main-white w-full h-[1px] inline-block flex-1"></span>
-          </h2>
-          {headerChildren}
-        </header>
-        {children}
-      </div>
+    <section
+      className={`flex flex-col gap-8 max-w-7xl mx-auto w-full ${
+        noInlinePadding ? "" : "px-4 md:px-8 lg:px-10 2xl:px-0"
+      }`}
+    >
+      <header className="flex flex-col gap-4 md:gap-8">
+        <h2
+          className={`uppercase font-medium  flex items-center justify-between gap-12 ${
+            titleCustomSizes
+              ? titleCustomSizes
+              : "text-2xl lg:text-5xl md:text-4xl"
+          }`}
+        >
+          {titulo}
+          <span className="bg-main-white w-full h-[1px] inline-block flex-1"></span>
+        </h2>
+        {headerChildren}
+      </header>
+      {children}
     </section>
   );
 }
