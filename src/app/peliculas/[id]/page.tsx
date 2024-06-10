e87e9558523657e56dab0ca04ac4df4c726933de
Diego Aldao <diegofrontdev@gmail.com>
2024-06-10 12:49:22 -0300

    añadido el componente ScrollToTop y la clase min-h-screen al main

--- src/app/peliculas/[id]/page.tsx
+++ src/app/peliculas/[id]/page.tsx
@@ -13,6 +13,7 @@ import MainInfoHero from "@/components/PageID/SeriesYPeliculas/Hero/MainInfoHero
 import fetchData from "@/data/fetchData";
 import { Suspense } from "react";
 import SkeletonRedes from "@/components/skeletons/PagePeliculasSeries/SkeletonRedes";
+import ScrollToTop from "@/components/ScrollToTop";
 
 export default async function DetallePelicula({
   params,
@@ -25,53 +26,52 @@ export default async function DetallePelicula({
   );
 
   return (
-    <main>
-      <>
-        <Hero
-          descripcion={pelicula.overview}
-          imagenesHero={
-            <ImagenesHero
-              urlPoster={pelicula.poster_path}
-              urlBackdrop={pelicula.backdrop_path}
-            />
-          }
-        >
-          <>
-            <HeaderHero
-              titulo={pelicula.title}
-              tagline={pelicula.tagline}
-              votoPromedio={pelicula.vote_average}
-              cantidadDeVotos={pelicula.vote_count}
-            />
-            <MainInfoHero
-              generos={pelicula.genres}
-              estreno={pelicula.release_date}
-              duracion={pelicula.runtime}
-            />
-          </>
-        </Hero>
-        <section className="main-content px-4 md:px-8 lg:px-10 max-w-7xl mx-auto 2xl:px-0 pb-20 grid w-full gap-12 lg:grid-cols-[2.5fr,1fr] lg:gap-x-4 xl:gap-x-8">
-          <Reparto cast={pelicula.credits.cast} />
-          <Media
-            id={id}
-            idiomaOriginal={pelicula.original_language}
-            mediaType="movie"
+    <main className="min-h-screen">
+      <ScrollToTop />
+      <Hero
+        descripcion={pelicula.overview}
+        imagenesHero={
+          <ImagenesHero
+            urlPoster={pelicula.poster_path}
+            urlBackdrop={pelicula.backdrop_path}
           />
-          <Similares similares={pelicula.similar} />
-          <aside className="lg:row-start-1 lg:col-start-2 flex flex-col gap-8">
-            <Suspense fallback={<SkeletonRedes />}>
-              <Redes id={id} />
-            </Suspense>
-            <SubInfo
-              tituloOriginal={pelicula.original_title}
-              estado={pelicula.status}
-              idiomaOriginal={pelicula.original_language}
-              presupuesto={pelicula.budget}
-              ingresos={pelicula.revenue}
-            />
-          </aside>
-        </section>
-      </>
+        }
+      >
+        <>
+          <HeaderHero
+            titulo={pelicula.title}
+            tagline={pelicula.tagline}
+            votoPromedio={pelicula.vote_average}
+            cantidadDeVotos={pelicula.vote_count}
+          />
+          <MainInfoHero
+            generos={pelicula.genres}
+            estreno={pelicula.release_date}
+            duracion={pelicula.runtime}
+          />
+        </>
+      </Hero>
+      <section className="main-content px-4 md:px-8 lg:px-10 max-w-7xl mx-auto 2xl:px-0 grid w-full gap-12 lg:grid-cols-[2.5fr,1fr] lg:gap-x-4 xl:gap-x-8">
+        <Reparto cast={pelicula.credits.cast} />
+        <Media
+          id={id}
+          idiomaOriginal={pelicula.original_language}
+          mediaType="movie"
+        />
+        <Similares similares={pelicula.similar} mediaType="movie" />
+        <aside className="lg:row-start-1 lg:col-start-2 flex flex-col gap-8">
+          <Suspense fallback={<SkeletonRedes />}>
+            <Redes id={id} />
+          </Suspense>
+          <SubInfo
+            tituloOriginal={pelicula.original_title}
+            estado={pelicula.status}
+            idiomaOriginal={pelicula.original_language}
+            presupuesto={pelicula.budget}
+            ingresos={pelicula.revenue}
+          />
+        </aside>
+      </section>
     </main>
   );
 }
