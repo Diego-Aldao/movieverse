4f52c3196a3767aa2615592fda2b17b87843f4f2
Diego Aldao <diegofrontdev@gmail.com>
2024-06-10 22:38:08 -0300

    mejorados los estilos de los botones y el input

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -23,32 +23,32 @@ export default function SearchBar() {
   return (
     <>
       <button
-        className={`w-7 h-7 transition-colors relative  ${
+        className={`w-7 h-7 lg:w-8 lg:h-8 xl:w-9 xl:h-9 transition-colors relative hover:text-main-color  ${
           searchVisible ? "text-main-color" : "text-main-white"
         }`}
         onClick={handleSearchBar}
       >
-        <span className="h-7 w-7 icon-[mdi--search] text-inherit"></span>
+        <span className="w-7 h-7 lg:w-8 lg:h-8 xl:w-9 xl:h-9 icon-[mdi--search] text-inherit"></span>
       </button>
       <div
-        className={`busqueda transition-all h-fit w-full absolute overflow-hidden right-0   ${
+        className={`busqueda transition-all h-8 md:h-10 lg:h-12 w-full absolute overflow-hidden right-0   ${
           searchVisible
-            ? "opacity-100 visible -bottom-[45px] md:-bottom-[65px]"
-            : "opacity-0 invisible -bottom-[35px] md:-bottom-[55px]"
+            ? "opacity-100 visible -bottom-[45px] md:-bottom-[55px] lg:-bottom-[75px]"
+            : "opacity-0 invisible -bottom-[40px] md:-bottom-[50px] lg:-bottom-[70px]"
         }`}
       >
         <form
-          className="w-full max-w-[400px] md:max-w-[600px] lg:max-w-[700px] md:mx-auto md:py-2 ml-auto h-full overflow-hidden bg-main-white p-2 py-1 flex gap-2 items-center rounded-md bg-opacity-10"
+          className="w-full max-w-[400px] md:max-w-[600px] lg:max-w-[700px] mx-auto  ml-auto h-full overflow-hidden bg-main-black bg-opacity-35 flex gap-1 items-center rounded-full"
           onSubmit={handleNavigate}
         >
           <button
             disabled={searchValue ? false : true}
-            className={`relative transition-all h-7 bg-main-white bg-opacity-15 p-1 rounded-md flex items-center  ${
+            className={`relative transition-all h-full px-2 md:px-4 rounded-md flex items-center  ${
               searchVisible ? "top-0 opacity-100" : "top-4 opacity-0"
             } ${
               searchValue
-                ? "text-main-color opacity-100 gap-2"
-                : "text-main-white opacity-75 gap-0"
+                ? "text-main-color gap-2 bg-main-black bg-opacity-15 hover:bg-opacity-25"
+                : "text-main-white gap-0"
             }`}
             onClick={handleNavigate}
           >
@@ -61,9 +61,8 @@ export default function SearchBar() {
             >
               buscar
             </span>
-            <span className="icon-[mdi--search] h-5 w-5 text-inherit"></span>
+            <span className="icon-[mdi--search] h-5 w-5 lg:h-6 lg:w-6 text-inherit"></span>
           </button>
-
           <input
             name=""
             id=""
@@ -72,15 +71,15 @@ export default function SearchBar() {
               setSearchValue(e.target.value);
             }}
             placeholder="Duna, Attack on titan, Sydney Sweeney....."
-            className="bg-transparent placeholder:text-secondary-white placeholder:opacity-75 px-1 w-full h-7  text-main-white outline-0 outline-offset-0"
+            className="bg-transparent placeholder:text-secondary-white placeholder:opacity-75 px-1 w-full h-full  text-main-white outline-0 outline-offset-0 placeholder:text-sm md:placeholder:text-base text:sm md:text-base"
           />
           <div
-            className={`transition-all relative h-7 cursor-pointer bg-main-white bg-opacity-15 p-1 rounded-md ${
+            className={`transition-all h-full hover:bg-main-black hover:bg-opacity-15 group px-2 rounded-md flex items-center cursor-pointer ${
               searchVisible ? "top-0 opacity-100" : "top-4 opacity-0"
             }`}
           >
             <span
-              className="h-5 w-5 icon-[mdi--close] hover:text-red-400"
+              className="h-4 w-4 lg:h-5 lg:w-5 icon-[mdi--close] transition-colors text-main-white group-hover:text-red-500"
               onClick={handleSearchBar}
             ></span>
           </div>
