fa9dca1ffcf9877697fda016a65b1ebc334db86b
Diego Aldao <diegofrontdev@gmail.com>
2024-06-13 21:56:32 -0300

    mejorada la apariencia para ir en sintonia con el componente SecondaryButton y los Tags

--- src/components/buttons/MainButton.tsx
+++ src/components/buttons/MainButton.tsx
@@ -17,13 +17,13 @@ export default function MainButton({
   return (
     <Link
       href={destino}
-      className={`w-fit text-main-white self-end px-3 py-1 bg-[#2425267e] border-main-white border-opacity-20 border rounded-full flex items-center gap-2 hover:border-opacity-80 transition-colors ${customStyles} group hover:border-main-color hover:text-main-color`}
+      className={`w-fit text-main-white self-end px-3 py-1 flex items-center gap-2 ${customStyles} group hover:border-main-white/75 transition-[border-color,box-shadow] cursor-pointer shadow-md shadow-transparent hover:shadow-main-black/50 bg-main-black/35 rounded-full border border-main-white/35 backdrop-blur-sm`}
     >
       <span className="first-letter:uppercase text-inherit md:pb-[2px]">
         {nombre}
       </span>
       <span
-        className={`${icon} h-4 w-4 ${customIconStyles} text-inherit transition-transform group-hover:translate-x-1 group-hover:text-main-color translate-x-0`}
+        className={`${icon} h-4 w-4 ${customIconStyles} text-inherit transition-transform group-hover:translate-x-1 translate-x-0`}
       ></span>
     </Link>
   );
