c43e7f76b28d69b7f0f8b731a79f702c8db74039
Diego Aldao <diegofrontdev@gmail.com>
2024-06-16 20:30:05 -0300

    simplificado el componente, ahora recibe por props las funciones para updatear o resetear los filtros. Corregidos los estilos

--- src/components/PagesPeliculasSeries/Filtros/Botones.tsx
+++ src/components/PagesPeliculasSeries/Filtros/Botones.tsx
@@ -1,57 +1,40 @@
-import { FiltrosFetch } from "@/types/localTypes";
-import { objectToString } from "@/utils/objectToString";
 import React from "react";
 
 interface Props {
-  updateUrl: (filtros: FiltrosFetch) => void;
   updateFilters: (pageNumber: number) => void;
-  filtros: FiltrosFetch;
-  initialFiltros: FiltrosFetch;
-  setFiltros: React.Dispatch<React.SetStateAction<FiltrosFetch>>;
   ordenVisible: boolean;
   filtrosVisible: boolean;
+  resetFilters: () => void;
 }
 
 export default function Botones({
-  updateUrl,
   updateFilters,
-  filtros,
-  initialFiltros,
-  setFiltros,
   ordenVisible,
   filtrosVisible,
+  resetFilters,
 }: Props) {
-  const resetFilters = () => {
-    setFiltros(initialFiltros);
-    updateUrl(initialFiltros);
-  };
   return (
     <div
-      className={`botones w-full flex gap-4  justify-evenly md:relative lg:gap-12 lg:w-fit lg:min-w-[944px] mx-auto ${
+      className={`botones w-full flex gap-4 transition-all justify-evenly md:relative lg:gap-12 lg:w-fit lg:min-w-[944px] mx-auto ${
         ordenVisible && !filtrosVisible && "sm:mt-[120px] xl:mt-[80px]"
       } ${
         filtrosVisible &&
         !ordenVisible &&
-        "sm:mt-[300px] lg:mt-[270px] xl:mt-[240px]"
+        "sm:mt-[500px] lg:mt-[370px] xl:mt-[380px]"
       } ${
         filtrosVisible &&
         ordenVisible &&
-        "sm:mt-[415px] lg:mt-[385px] xl:mt-[320px] 2xl:mt-[320px]"
+        "sm:mt-[615px] lg:mt-[490px] xl:mt-[460px]"
       }`}
     >
       <button
         onClick={resetFilters}
-        disabled={filtros === initialFiltros}
-        className={` sm:uppercase  rounded-sm w-full max-w-[300px] capitalize text-sm px-2 border text-main-white py-1 font-semibold sm:text-base sm:px-4 md:flex-1 md:max-w-[350px] ${
-          filtros === initialFiltros
-            ? "bg-main-black opacity-50 border-secondary-black"
-            : "bg-secondary-black opacity-100 border-main-color"
-        }`}
+        className="sm:uppercase border-main-white/15 rounded-full w-full max-w-[300px] capitalize text-sm px-2 border text-main-white/75 hover:text-main-white py-1 font-semibold sm:text-base sm:px-4 md:flex-1 md:max-w-[350px] hover:border-main-white/50 transition-colors"
       >
         resetear filtros
       </button>
       <button
-        className="px-2 py-1 text-sm font-semibold sm:uppercase w-full max-w-[300px] capitalize bg-main-color text-main-black rounded-sm sm:text-base sm:px-4 md:flex-1 md:max-w-[350px]"
+        className="px-2 py-1 text-sm font-semibold sm:uppercase w-full max-w-[300px] capitalize bg-main-color hover:text-main-black rounded-full border hover:border-main-color border-transparent transition-colors text-main-black/75 sm:text-base sm:px-4 md:flex-1 md:max-w-[350px]"
         onClick={() => {
           updateFilters(1);
         }}
