3c96c929a55a04c15e13b1f3179822929e7258e7
Diego Aldao <diegofrontdev@gmail.com>
2024-06-17 11:49:27 -0300

    corregidas las clases del boton para que quede acorde a los nuevos estilos de los demas botones de la pagina

--- src/components/PagesPeliculasSeries/Filtros/FilterDropdown.tsx
+++ src/components/PagesPeliculasSeries/Filtros/FilterDropdown.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useState } from "react";
+import React, { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
@@ -23,21 +23,27 @@ export default function FilterDropdown({
     <div className="filtrar flex flex-col gap-2">
       <button
         onClick={toggleDropdown}
-        className={`header gap-2 flex min-w-[200px] items-center justify-between p-2 bg-secondary-black rounded-sm border transition-colors ${
-          dropdownVisible ? "border-main-color" : "border-transparent"
+        className={`header gap-2 flex min-w-[200px] items-center justify-between p-2 px-4 hover:border-main-white/50 bg-secondary-black rounded-full border transition-colors ${
+          dropdownVisible ? "border-main-white/50" : "border-transparent"
         }`}
       >
-        <span className="capitalize">{nombreFiltro}</span>
+        <span
+          className={`capitalize transition-colors ${
+            dropdownVisible ? "text-main-white" : "text-main-white/90"
+          }`}
+        >
+          {nombreFiltro}
+        </span>
         <span
           className={`icon-[mdi--chevron-down] transition-[transform,colors] w-6 h-6 ${
             dropdownVisible
-              ? "rotate-180 text-main-color"
-              : "rotate-0 text-main-white"
+              ? "rotate-180 text-main-white"
+              : "rotate-0 text-main-white/75"
           }`}
         ></span>
       </button>
       <div
-        className={`bg-secondary-black z-[3] w-full sm:absolute left-0 transition-all rounded-sm  ${
+        className={`bg-secondary-black z-[3] w-full sm:absolute left-0 transition-opacity rounded-sm  ${
           dropdownVisible
             ? "visible opacity-100 h-fit p-2 py-4 sm:p-4 sm:py-6"
             : "invisible opacity-0 h-0 p-0"
