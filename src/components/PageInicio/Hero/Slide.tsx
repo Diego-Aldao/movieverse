cea2063d6cf3adb7834201c47916d7e9d3f5c772
Diego Aldao <diegofrontdev@gmail.com>
2024-06-17 15:42:06 -0300

    añadido rootMargin a los objetos del custom hook useInView. Añadido el componente userInteraction para guardar o likear la pelicula y corregidas/reordenadas las clases tailwind para que funcionen con los nuevos rootMargins

--- src/components/PageInicio/Hero/Slide.tsx
+++ src/components/PageInicio/Hero/Slide.tsx
@@ -1,4 +1,5 @@
 import MainButton from "@/components/buttons/MainButton";
+import UserInteraction from "@/components/cards/UserInteraction";
 import { numberToFixed } from "@/utils/fixedNumbers";
 import React from "react";
 import { useInView } from "react-intersection-observer";
@@ -20,50 +21,61 @@ export default function Slide({
   id,
   children,
 }: Props) {
-  const [ref, inView] = useInView({ threshold: 0 });
-  const [ref2, inView2] = useInView({ threshold: 0 });
+  const [ref, inView] = useInView({
+    threshold: 0,
+    rootMargin: "-50px -150px -50px -150px",
+  });
+  const [ref2, inView2] = useInView({
+    threshold: 0,
+    rootMargin: "-50px -150px -50px -150px",
+  });
   const [ref3, inView3] = useInView({ threshold: 0 });
 
   return (
-    <div className="slide bg-main-color after:min-h-[700px] after:lg:min-h-[800px] lg:px-10 w-full h-full p-4 md:px-8">
-      <div className="contenido-slide z-10 relative flex flex-col w-full h-full justify-end gap-3 max-w-7xl mx-auto">
+    <div className="slide bg-secondary-black  w-full h-full px-4 md:px-8 lg:px-10 ">
+      <div className="contenido-slide z-[2] relative flex flex-col justify-end w-full h-full max-w-7xl mx-auto cursor-default">
         <div
           data-swiper-parallax="-55%"
-          className="about flex flex-col gap-4 mb-36 md:mb-40"
+          className="about flex flex-col w-full h-fit mb-28 gap-4 "
         >
-          <header className="flex flex-col gap-2 justify-center">
-            <div
-              ref={ref2}
-              className={`transition-all flex items-center gap-2 w-full relative ${
-                inView2 ? "left-0 opacity-100" : "-left-4 opacity-0"
-              }`}
-            >
-              <span className="icon-[mdi--star] md:h-6 md:w-6 text-main-color"></span>
-              <span className="md:text-xl">{numberToFixed(votoPromedio)}</span>
+          <header
+            ref={ref}
+            className={`flex flex-col gap-2 justify-center w-fit relative transition-all  ${
+              inView ? "opacity-100 top-0" : "top-4 opacity-0"
+            }`}
+          >
+            <div className="upper flex gap-8 w-fit items-center">
+              <div className={`transition-all flex items-center gap-2 w-full`}>
+                <span className="icon-[mdi--star] md:h-6 md:w-6 text-main-color"></span>
+                <span className="md:text-xl">
+                  {numberToFixed(votoPromedio)}
+                </span>
+              </div>
+              <UserInteraction />
             </div>
+
             <h1
-              ref={ref}
-              className={`transition-all delay-150 w-fit text-3xl uppercase font-semibold sm:text-4xl md:text-5xl lg:text-6xl xl:text-6xl max-w-[300px] sm:max-w-[450px] lg:max-w-[550px] ${
-                inView ? "opacity-100 top-0" : "top-4 opacity-0"
-              } relative`}
+              className={`transition-all line-clamp-2 delay-150 w-fit text-3xl uppercase font-semibold sm:text-4xl md:text-5xl  xl:text-6xl max-w-[300px] sm:max-w-[550px] md:max-w-[700px] lg:max-w-[850px]`}
             >
               {titulo}
             </h1>
           </header>
           {generos}
-          <div className="max-w-[500px] md:max-w-[550px] flex flex-col gap-5">
-            <p
-              ref={ref3}
-              className={`lg:text-lg transition-all delay-75 line-clamp-4 relative ${
-                inView3 ? "opacity-100 bottom-0" : "opacity-0 -bottom-10"
-              }`}
-            >
-              {descripcion}
-            </p>
+          <div
+            ref={ref2}
+            className={`transition-all relative flex flex-col gap-5 max-w-[500px] md:max-w-[600px] lg:max-w-[600px] xl:max-w-[700px] ${
+              inView2 ? "opacity-100 bottom-0" : "opacity-0 bottom-4"
+            }`}
+          >
+            <p className="lg:text-lg line-clamp-3">{descripcion}</p>
             <MainButton
-              destino={`/detalle?id=${id}`}
+              destino={`/peliculas/${id}`}
               icon="icon-[mdi--arrow-right-thin]"
               nombre="ver detalle"
+              customIconStyles="lg:h-6 lg:w-6"
+              customStyles={`${
+                inView2 ? "opacity-100 left-0" : "opacity-0 -left-48"
+              } delay-150 relative transition-all`}
             />
           </div>
         </div>
