178cf0c61cd9c66660525590d5d2669aa124b1f1
Diego Aldao <diegofrontdev@gmail.com>
2024-06-17 15:36:19 -0300

    añadidos los componentes MainTag y SecondaryButton para reemplazar el span y el button

--- src/components/PageInicio/Hero/Hero.tsx
+++ src/components/PageInicio/Hero/Hero.tsx
@@ -1,19 +1,22 @@
 import React from "react";
 import Slider from "./Slider";
+import MainTag from "@/components/tags/MainTag";
+import SecondaryButton from "@/components/buttons/SecondaryButton";
 
 export default async function Hero() {
   return (
     <section className="hero h-screen min-h-[830px] relative">
-      <span className="absolute top-28 md:top-32 lg:top-44  z-20 left-4 md:left-8 lg:left-10 uppercase text-xs md:text-sm bg-[#2425267e] py-2 px-4 w-fit rounded-full">
+      <MainTag customStyles="absolute top-28 md:top-32 lg:top-44 py-2 z-20 left-4 md:left-8 lg:left-10 uppercase cursor-default">
         últimos estrenos
-      </span>
+      </MainTag>
       <Slider />
-      <button className="absolute z-20 bottom-4 left-[calc(50%-50px)] lg:left-[calc(50%-65px)] flex items-center gap-1 py-2 lg:px-4 px-3">
-        <span className="capitalize ml-auto md:text-lg lg:text-xl">
-          explorar
-        </span>
-        <span className="icon-[mdi--arrow-down-thin] h-5 w-5 md:h-6 md:w-6 lg:h-7 lg:w-7"></span>
-      </button>
+      <SecondaryButton
+        destino="#tendencias"
+        nombre="explorar"
+        icon="icon-[mdi--arrow-down-thin]"
+        customIconStyles="h-5 w-5 md:h-6 md:w-6"
+        customStyles="absolute z-20 bottom-4 left-0 right-0 mx-auto w-fit flex items-center gap-1 "
+      />
     </section>
   );
 }
