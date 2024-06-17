3f943be12f1d025a7c561741492a5b4ebfacb1f4
Diego Aldao <diegofrontdev@gmail.com>
2024-06-17 17:51:15 -0300

    añadido un componente para renderizar el copyright del footer

---
+++ src/components/layout/Footer/Copy.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Copy() {
+  return (
+    <span className="copy w-full text-center uppercase text-xs pt-10 text-secondary-white">
+      movieverse, derechos reservados 2024 &copy;
+    </span>
+  );
+}
