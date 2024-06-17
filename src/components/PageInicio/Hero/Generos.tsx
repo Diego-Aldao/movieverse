e46ff8332cee5f6ae4e6eb7cc9a8daee0b61964d
Diego Aldao <diegofrontdev@gmail.com>
2024-06-17 15:35:17 -0300

    añadido el componente mainTag que reemplaza los estilos del li, y añadido un rootMargin al objeto del customHook useInView

--- src/components/PageInicio/Hero/Generos.tsx
+++ src/components/PageInicio/Hero/Generos.tsx
@@ -1,29 +1,30 @@
 import React from "react";
 import listadoGeneros from "@/generos.json";
 import { useInView } from "react-intersection-observer";
+import MainTag from "@/components/tags/MainTag";
 
 interface Props {
   generos: number[] | undefined;
 }
 
 export default function Generos({ generos }: Props) {
-  const [ref, inView] = useInView({ threshold: 0 });
+  const [ref, inView] = useInView({
+    threshold: 0,
+    rootMargin: "-50px -150px -50px -150px",
+  });
   const currentGeneros = listadoGeneros.filter((genero) => {
     return generos?.includes(genero.id);
   });
   return (
     <ul
       ref={ref}
-      className={`generos transition-all delay-150 relative flex items-center gap-2 ${
-        inView ? "bottom-0 opacity-100" : "bottom-10 opacity-0"
+      className={`generos transition-all delay-75 relative flex items-center gap-2 w-fit ${
+        inView ? "bottom-0 opacity-100" : "-bottom-4 opacity-0"
       }`}
     >
       {currentGeneros.slice(0, 3).map((genero) => (
-        <li
-          className="capitalize px-3 py-1 text-xs bg-[#2425267e] border-main-white border-opacity-50 border rounded-full md:px-4 md:text-sm"
-          key={genero.id}
-        >
-          {genero.name}
+        <li key={genero.id}>
+          <MainTag>{genero.name}</MainTag>
         </li>
       ))}
     </ul>
