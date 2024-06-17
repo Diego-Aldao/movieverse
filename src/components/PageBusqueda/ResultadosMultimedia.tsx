d5900e6edcd0781865a86bd53658afb0ca41c86e
Diego Aldao <diegofrontdev@gmail.com>
2024-06-17 16:11:32 -0300

    actualizadas las props que ahora pide el componente MiniCard

--- src/components/PageBusqueda/ResultadosMultimedia.tsx
+++ src/components/PageBusqueda/ResultadosMultimedia.tsx
@@ -19,25 +19,25 @@ export default function ResultadosMultimedia({
       noInlinePadding={true}
       titleCustomSizes="text-xl sm:text-2xl md:text-3xl lg:text-4xl "
     >
-      <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full">
         {resultados.map((resultado) => (
           <MiniCard
-            pathImagen={resultado.backdrop_path || resultado.poster_path}
-            nombre={resultado.title || resultado.name || ""}
-            hasButton={true}
-            destino={`/${media_type}/${resultado.id}`}
             key={resultado.id}
-            customStyles="h-[170px] max-h-[170px] md:max-w-[400px]"
+            nombre={resultado.name || resultado.title || ""}
+            id={resultado.id}
+            pathImagen={resultado.backdrop_path || ""}
+            media_type={
+              resultado.media_type === "movie" ? "peliculas" : "series"
+            }
+            simple={true}
+            customStyles="h-[140px] max-h-[140px] md:max-w-[400px]"
           >
-            <>
-              <Generos
-                tipo="movie"
-                listaGenerosNumericos={resultado.genre_ids || []}
-                cantidad={2}
-                stylesItem="text-xs"
-              />
-              <p className="line-clamp-2 text-xs">{resultado.overview}</p>
-            </>
+            <Generos
+              tipo="movie"
+              listaGenerosNumericos={resultado.genre_ids || []}
+              cantidad={2}
+              stylesItem="text-xs"
+            />
           </MiniCard>
         ))}
       </div>
