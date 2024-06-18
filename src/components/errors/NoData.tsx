a9553c10dd14015751d266f47d4771134b9ed1e3
Diego Aldao <diegofrontdev@gmail.com>
2024-06-18 00:58:56 -0300

    ahora recibe el "mensaje" y estilos custom por props

--- src/components/errors/NoData.tsx
+++ src/components/errors/NoData.tsx
@@ -3,19 +3,21 @@ import { useInView } from "react-intersection-observer";
 
 interface Props {
   children?: React.ReactNode;
+  nombre: string;
+  customStyles?: string;
 }
 
-export default function NoData({ children }: Props) {
+export default function NoData({ children, nombre, customStyles }: Props) {
   const [ref, inView] = useInView({ threshold: 0, triggerOnce: true });
   return (
     <div
       ref={ref}
       className={`min-h-[200px] lg:min-h-[400px] flex items-center justify-center w-full col-span-full relative transition-all delay-200 ${
         inView ? "opacity-100 top-0" : "opacity-0 top-10"
-      }`}
+      } ${customStyles}`}
     >
       <span className="first-letter:uppercase text-center w-fit text-xl md:text-2xl inline-block line-clamp-1 ">
-        no se encontraron resultados
+        {nombre}
       </span>
       {children}
     </div>
