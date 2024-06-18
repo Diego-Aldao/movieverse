7cfa1ba87dfbc312fe23b9d679d03abbcbf4c0cb
Diego Aldao <diegofrontdev@gmail.com>
2024-06-18 16:47:32 -0300

    aghora el logo lleva el slogan

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
     <header className="w-full pointer-events-none px-4 md:px-8 lg:px-10 pt-4 md:pt-6 lg:pt-8 h-24 lg:h-48 absolute top-0 left-0 z-50 bg-gradient-to-b from-[#18191ac2] to-transparent">
       <nav className="flex items-center justify-between pointer-events-auto relative xl:gap-2 2xl:gap-4">
         <div className="xl:flex-1">
-          <Logo />
+          <Logo sloganOn={true} />
         </div>
         <ul className="nav-desktop hidden md:flex items-center gap-12 xl:flex-1">
           {INTERNAL_NAVIGATION.slice(1, 4).map((item) => (
