0ad6d9c04f912196a8a816d3a4370eaa4ab8b047
Diego Aldao <diegofrontdev@gmail.com>
2024-06-18 18:33:24 -0300

    añadido el componente UserInteraction. Ahora el componente recibe las props que necesita pasarle al userIntreraction para que funcione

--- src/components/PageID/SeriesYPeliculas/Hero/HeaderHero.tsx
+++ src/components/PageID/SeriesYPeliculas/Hero/HeaderHero.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { numberToFixed } from "@/utils/fixedNumbers";
+import UserInteraction from "@/components/cards/UserInteraction";
 
 interface Props {
   votoPromedio: number;
   cantidadDeVotos: number;
   titulo: string;
   tagline: string;
+  id: number;
+  poster: string;
+  media_type: string;
 }
 
 export default function HeaderHero({
@@ -13,19 +17,32 @@ export default function HeaderHero({
   cantidadDeVotos,
   titulo,
   tagline,
+  id,
+  poster,
+  media_type,
 }: Props) {
   return (
     <header className="flex flex-col justify-center">
-      {votoPromedio !== undefined && cantidadDeVotos ? (
-        <div className={`flex items-center gap-2 w-full relative mb-2`}>
-          <span className="icon-[mdi--star] md:h-6 md:w-6 text-main-color"></span>
-          <span className="md:text-xl">{numberToFixed(votoPromedio)}</span>
-        </div>
-      ) : (
-        <span className="uppercase text-xs px-3 py-1 bg-[#2425267e] rounded-full w-fit mb-2">
-          sin calificar
-        </span>
-      )}
+      <div className="header flex gap-4 md:gap-8 items-center w-fit mb-2">
+        {votoPromedio !== undefined && cantidadDeVotos ? (
+          <div className={`flex items-center gap-1 w-full relative `}>
+            <span className="icon-[mdi--star] md:h-6 md:w-6 text-main-color"></span>
+            <span className="md:text-xl">{numberToFixed(votoPromedio)}</span>
+          </div>
+        ) : (
+          <span className="uppercase text-xs px-3 py-1 bg-[#2425267e] rounded-full w-fit mb-2">
+            sin calificar
+          </span>
+        )}
+        <UserInteraction
+          item={{
+            id: id,
+            nombre: titulo,
+            img_path: poster,
+            media_type: media_type,
+          }}
+        />
+      </div>
 
       <h1
         className={`w-fit text-3xl uppercase font-semibold sm:text-4xl md:text-5xl lg:text-6xl xl:text-6xl tracking-tight max-w-[300px] sm:max-w-[550px] md:max-w-[700px] lg:max-w-[850px] relative`}
