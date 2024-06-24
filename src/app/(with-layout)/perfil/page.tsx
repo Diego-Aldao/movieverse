df3db865c24ca8f72947d01706865f4e174d6b1c
Diego Aldao <diegofrontdev@gmail.com>
2024-06-24 14:28:46 -0300

    quitado el componente coleccion

--- src/app/(with-layout)/perfil/page.tsx
+++ src/app/(with-layout)/perfil/page.tsx
@@ -3,14 +3,6 @@ import React from "react";
 import banner from "@/assets/banner.png";
 import profile from "@/assets/perfil.webp";
 import NoData from "@/components/errors/NoData";
-import dynamic from "next/dynamic";
-
-const DynamicColeccion = dynamic(
-  () => import("@/components/PagePerfil/Coleccion"),
-  {
-    ssr: false,
-  }
-);
 
 export default function Perfil() {
   return (
@@ -60,7 +52,6 @@ export default function Perfil() {
         </div>
       </header>
       <section className="section px-4 md:px-8 lg:px-10 max-w-7xl 2xl:px-0 mx-auto grid gap-8 lg:grid-cols-[70%,1fr] lg:gap-4 pb-10">
-        <DynamicColeccion />
         <aside>
           <div>
             <header className="flex flex-col gap-4 md:gap-8">
