7fc7b673faa708e4cb2f50f31fe924ad059517e9
Diego Aldao <diegofrontdev@gmail.com>
2024-06-24 13:03:12 -0300

    añadidos los types para los filtros de las paginas peliculas y series, para los generos locales y las colecciones (favoritos y guardados)

--- src/types/localTypes.ts
+++ src/types/localTypes.ts
@@ -2,6 +2,16 @@ export interface Filtro {
   tiempo: string;
   tipo: string;
 }
+export interface ValoresFiltros {
+  [key: string]: string;
+}
+
+export interface FiltroPerfil {
+  id: number;
+  nombre: string;
+  valor: string;
+}
+
 export interface MiniaturaMedia {
   id: string;
   key: string;
@@ -12,4 +22,29 @@ export interface MiniaturaMedia {
 
 export interface RedesSociales {
   [key: string]: string | null | number;
+}
+
+export interface FiltrosFetch {
+  page: number;
+  sort_by: string;
+  with_genres?: string;
+  "primary_release_date.gte"?: string;
+  "primary_release_date.lte"?: string;
+  "vote_average.gte"?: number;
+  "vote_average.lte"?: number;
+  "vote_count.gte"?: number;
+  "with_runtime.gte"?: number;
+  "with_runtime.lte"?: number;
+}
+
+export interface Coleccion {
+  id: number;
+  nombre: string;
+  img_path: string;
+  media_type: string;
+}
+
+export interface Genero {
+  id: number;
+  name: string;
 }
