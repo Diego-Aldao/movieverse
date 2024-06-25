8922051bf186b500a354e224337f3aace8dcacc2
Diego Aldao <diegofrontdev@gmail.com>
2024-06-25 19:30:40 -0300

    agregada una separacion entra los componentes

--- src/components/PagePerfil/Coleccion.tsx
+++ src/components/PagePerfil/Coleccion.tsx
@@ -3,7 +3,7 @@ import Guardados from "./Guardados";
 
 export default function Coleccion() {
   return (
-    <div className="col-start-1 w-full">
+    <div className="col-start-1 w-full flex flex-col gap-12">
       <Favoritos />
       <Guardados />
     </div>
